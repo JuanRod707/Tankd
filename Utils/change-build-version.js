93f0edf5e463b8e2894c1dea8fc2d50b0caa2c36
Juan Rodriguez <juan.rodriguez@etermax.com.uy>
2018-03-13 13:55:03 -0300

    asd

--- Utils/change-build-version.js
+++ Utils/change-build-version.js
@@ -1,11 +1,23 @@
-var execSync = require("child_process").execSync;
-var path = require("path");
-var fs = require("fs");
-var outputFile = path.normalize("./package.json");
-var currentPackage = JSON.parse(fs.readFileSync('./package.json', 'utf8');
+var execSync = require('child_process').execSync;
+var path = require('path');
+var fs = require('fs');
+var outputFile = path.normalize('./package.json');
+var currentPackage = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 
 console.log(currentPackage.version);    
-currentPackage.version;
+let versions = currentPackage.version.Split('.');
+
 
 fs.writeFileSync(outputFile);
-console.log("Succesfully updated " + outputFile);
+console.log('Succesfully updated ' + outputFile);
+
+var pkg = "{ 'name': '@etermax/chameleon','version': '1.0.0','author': 'ismaelserrada','dependencies': {'@etermax/etermax-ci': '2.1.1','@etermax/nfluent': '1.0.0','@etermax/unity-rx': 'latest'},'devDependencies': {'@etermax/upm-editor-plugin': 'latest'},'files': ['Assets/Package','CHANGELOG.md','.upm-config'],'license': 'UNLICENSED'}";
+var version = json.parse(pkg).version;
+
+var versions = version.split('.');
+var major = Number(versions[0]);
+major++;
+versions[0] = major;
+var output = versions.join('.');
+
+console.log(output);
